fb5e2121e91f75cec20d0fb4182ef0361cd246de
LUK0VSK1 <118302568+LUK0VSK1@users.noreply.github.com>
2022-12-23 09:27:35 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,11 +3,20 @@
 
 using namespace std;
 
+int sum(int sumNProgres, int aN)
+{
+
+	aN = firstNum + (n - 1) / d;
+	
+
+	sumNProgres = ((2 * firstNum + (n - 1) * d) / 2) * n;//сума перших n чисел
+}
+
 int main() {
 	setlocale(0, "");
 
 	int d;//різниця
-	int firstNum;//a1
+	int firstNum;//а1
 	int n;
 
 	cout << "d " << " firstNum " << " n " << endl;
@@ -15,15 +24,7 @@ int main() {
 	cout << "= " << "     =    " << " = " << endl;
 
 	cin >> d >> firstNum >> n;
-
-	int aN;//a n-ого номера
-
-	aN = firstNum + (n-1) / d;
-
-	int sumNProgres;
 	
-	sumNProgres = ((2*firstNum+(n-1)*d)/2)*n;//сума перших n чисел
-
 	cout << " n-го члена прогресії " << aN << endl << " сума перших n чисел " << sumNProgres;	
 	return 0;
 }
